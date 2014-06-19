c8fdd6a63ca46295f07671df5267eec7463f543b
Travis Mozden <tmmozden@mtu.edu>
2014-06-18 20:29:27 -0500

    working button

--- app/src/main/java/com/example/eventbasedalarmclocktest/app/DisplayMessageActivity.java
+++ app/src/main/java/com/example/eventbasedalarmclocktest/app/DisplayMessageActivity.java
@@ -1,31 +1,37 @@
 package com.example.eventbasedalarmclocktest.app;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.EditText;
-import android.view.View;
+import android.content.Intent;
+import android.widget.TextView;
 
 
-public class MainActivity extends Activity {
+public class DisplayMessageActivity extends Activity {
 
-    public final static String EXTRA_MESSAGE = "com.example.eventbasedalarmclock.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        System.out.println("I MADE IT HERE MAIN!!!!!");
+        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        setContentView(textView);
 
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.display_message, menu);
         return true;
     }
 
@@ -36,18 +42,9 @@ public class MainActivity extends Activity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
-            //return true;
+            return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    /** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class );
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
     }
 
 }
